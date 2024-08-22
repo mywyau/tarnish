0e2e83ff6a85de7ba676ddad70387389f2a69253
Michael Yau <47248104+mywyau@users.noreply.github.com>
2024-08-22 19:14:53 +0100

    delete-single update delete single blog post endpoint (#5)
    * delete-single update delete single post endpoint
    
    * update backend

--- src/blog_controller.rs
+++ src/blog_controller.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use actix_web::{delete, Error, get, HttpResponse, post, put, web};
+use actix_web::{delete, get, post, put, web, Error, HttpResponse};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 use dotenv::dotenv;
@@ -71,43 +71,6 @@ async fn create_post(
 }
 
 
-// #[post("/blog/post/create")]
-// async fn create_post(
-//     pool: web::Data<DbPool>,
-//     post: web::Json<PostInput>,
-// ) -> Result<HttpResponse, Error> {
-//     let post_input = post.into_inner();
-//
-//     let new_post = NewPost {
-//         post_id: post_input.post_id,
-//         title: post_input.title,
-//         body: post_input.body,
-//     };
-//
-//     let mut conn = pool.get().map_err(|e| {
-//         actix_web::error::ErrorInternalServerError(format!("Couldn't get db connection from pool: {}", e))
-//     })?;
-//
-//     conn.transaction::<_, diesel::result::Error, _>(|conn| {
-//         // // Check if the table is empty
-//         // let post_count: i64 = posts::table.count().get_result(conn)?;
-//         // if post_count == 0 {
-//         //     // Reset the sequence
-//         //     diesel::sql_query("ALTER SEQUENCE posts_id_seq RESTART WITH 1").execute(conn)?;
-//         // }
-//
-//         // Insert the new post
-//         diesel::insert_into(posts::table)
-//             .values(&new_post)
-//             .get_result::<Post>(conn)
-//             .map_err(|e| {
-//                 eprintln!("Error inserting new post: {:?}", e);
-//                 e
-//             })
-//     }).map_err(|e| actix_web::error::ErrorInternalServerError(format!("Transaction failed: {}", e)))
-//         .map(|post| HttpResponse::Created().json(post))
-// }
-
 #[get("/blog/post/retrieve/post-id/{post_id}")]
 async fn get_by_post_id(
     path: web::Path<String>,
@@ -158,7 +121,6 @@ async fn update_post(
     post: web::Json<PostInput>,
     pool: web::Data<DbPool>,
 ) -> Result<HttpResponse, Error> {
-
     let post_id = path.into_inner();
     let post_input = post.into_inner();
 
@@ -183,6 +145,7 @@ async fn update_post(
     }
 }
 
+
 #[delete("/blog/post/single/{post_id}")]
 async fn delete_post(
     path: web::Path<String>,  // Changed to String since post_id is a varchar
@@ -193,11 +156,45 @@ async fn delete_post(
         actix_web::error::ErrorInternalServerError(format!("Couldn't get db connection from pool: {}", e))
     })?;
 
-    match diesel::delete(posts::table.filter(posts::post_id.eq(post_id))).execute(&mut conn) {
-        Ok(_) => Ok(HttpResponse::Ok().finish()),
-        Err(e) => {
-            eprintln!("Error deleting post: {:?}", e);
-            Ok(HttpResponse::InternalServerError().finish())
+    // First, retrieve the title of the post before deleting it
+    let post_title =
+        posts::table
+            .filter(posts::post_id.eq(&post_id))
+            .select(posts::title)
+            .first::<String>(&mut conn)
+            .optional()
+            .map_err(|e| {
+                actix_web::error::ErrorInternalServerError(format!("Error retrieving post title: {}", e))
+            })?;
+
+    match post_title {
+        Some(title) => {
+            // Now delete the post
+            match diesel::delete(posts::table.filter(posts::post_id.eq(&post_id))).execute(&mut conn) {
+                Ok(_) => {
+                    let response_body = json!({
+                        "message": format!("Blog post '{}' has been deleted", title)
+                    });
+
+                    Ok(HttpResponse::Ok()
+                        .content_type("application/json")
+                        .json(response_body))
+                }
+                Err(e) => {
+                    eprintln!("Error deleting post: {:?}", e);
+                    Ok(HttpResponse::InternalServerError().finish())
+                }
+            }
+        }
+        None => {
+            // If no post with the given ID is found
+            let response_body = json!({
+                "error": format!("Blog post with ID '{}' not found", post_id)
+            });
+
+            Ok(HttpResponse::NotFound()
+                .content_type("application/json")
+                .json(response_body))
         }
     }
 }
